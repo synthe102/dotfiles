2c80755e13ebb1cd36e67a9f084b182aecac3633
Léonard Suslian <leonard@suslian.engineer>
2024-08-21 11:29:05 +0200

    feat(nix): enable automatic garbage collection

--- modules/default.nix
+++ modules/default.nix
@@ -7,6 +7,7 @@
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
+  nix.gc.automatic = true;
 
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
