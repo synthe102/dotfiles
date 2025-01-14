67bbde753f1519246e7e494d5b61d388dc09e5c9
synthe102 <leonard@suslian.engineer>
2025-01-14 10:53:30 +0100

    feat(nvim): use codestral with avante

--- homeManagerModules/neovim/plugins/avante.nix
+++ homeManagerModules/neovim/plugins/avante.nix
@@ -4,36 +4,16 @@
       require("avante_lib").load()
       require("avante").setup({
         provider = "mistral",
+        auto_suggestions_provider = "mistral",
+        behaviour = {
+          auto_suggestions = true,
+        },
         vendors = {
           mistral = {
-            endpoint = "https://codestral.mistral.ai/v1/chat/completions",
-            model = "codestral-latest",
+            __inherited_from = "openai",
             api_key_name = "CODESTRAL_API_KEY",
-            parse_curl_args = function(opts, code_opts)
-              local api_key = os.getenv(opts.api_key_name)
-              local Llm = require("avante.providers")
-
-              return {
-                url = opts.endpoint,
-                headers = {
-                  ["Accept"] = "application/json",
-                  ["Content-Type"] = "application/json",
-                  ["Authorization"] = "Bearer " .. api_key,
-                },
-                body = {
-                  model = opts.model,
-                  messages = Llm.openai.parse_message(code_opts),
-                  temperature = 0.7,
-                  max_tokens = 8192,
-                  stream = true,
-                  safe_prompt = false,
-                },
-              }
-            end,
-            parse_response_data = function(data_stream, event_state, opts)
-              local Llm = require("avante.providers")
-              Llm.openai.parse_response(data_stream, event_state, opts)
-            end,
+            endpoint = "https://api.mistral.ai/v1",
+            model = "codestral-latest",
           },
         },
       })
