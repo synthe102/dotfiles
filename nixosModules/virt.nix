629cd4a98371827274939a7df1d6a0b210ba1a73
synthe102 <leonard@suslian.engineer>
2025-10-18 11:25:05 +0000

    chore: remove deprecated ovmf conf

--- nixosModules/virt.nix
+++ nixosModules/virt.nix
@@ -9,16 +9,6 @@
       package = pkgs.qemu_kvm;
       runAsRoot = true;
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [
-          (pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          })
-          .fd
-        ];
-      };
     };
   };
   users.users.${user.name}.extraGroups = ["libvirtd"];
