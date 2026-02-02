ed9d7d81f3a5010c81f8a520fb9828a49e960408
synthe102 <leonard@suslian.engineer>
2026-02-02 10:38:33 +0100

    feat: gc and optimize on schedule

--- modules/default.nix
+++ modules/default.nix
@@ -11,7 +11,26 @@
 
   # Auto upgrade nix package and the daemon service.
   nix.package = pkgs.nix;
-  nix.gc.automatic = true;
+  nix.gc = {
+    automatic = true;
+    interval = {
+      Weekday = 0;
+      Hour = 2;
+      Minute = 0;
+    }; # Weekly on Sunday
+    options = "--delete-older-than 7d";
+  };
+
+  # Optional: auto optimize store
+  nix.optimise = {
+    automatic = true;
+    interval = {
+      Weekday = 0;
+      Hour = 3;
+      Minute = 0;
+    };
+  };
+
   nix.registry.nixpkgs.flake = inputs.nixpkgs;
 
   # Necessary for using flakes on this system.
