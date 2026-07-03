9fb92fb57f491277d056b31d76e0878105999d84
synthe102 <leonard@suslian.engineer>
2026-07-03 09:18:35 +0200

    feat(darwin): add secretive

--- homeManagerModules/shell/darwin.nix
+++ homeManagerModules/shell/darwin.nix
@@ -5,6 +5,9 @@
 }:
 {
   config = lib.mkIf pkgs.stdenv.isDarwin {
+    home.sessionVariables = {
+      SSH_AUTH_SOCK = "$HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
+    };
     programs.zsh = {
       shellAliases = {
         tailscale = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
